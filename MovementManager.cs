cbe1b82e89dcbe9c30ab5348995356f7b14f9513
DapZey <72968513+DapZey@users.noreply.github.com>
2024-02-15 22:57:15 +0200

    Update MovementManager.cs

--- MovementManager.cs
+++ MovementManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using Cinemachine;
-public class MovementManager : MonoBehaviour
+
+public class NewMovement : MonoBehaviour
 {
+    // Start is called before the first frame update
     public float playerMovementSpeed = 5;
     public float dashSpeed = 250f;
     public float dashCooldown = 1;
@@ -17,17 +21,19 @@ public class MovementManager : MonoBehaviour
     CharacterController playerInputController;
     [SerializeField] LayerMask playerGroundMask;
 
-    float gravityOnPlayer = -20f;
+    float gravityOnPlayer = -30f;
     Vector3 playerMovementVelocity;
     bool grounded;
     float jumpVelocity;
     float movementSpeed;
     bool isMovementLocked = false;
     public CinemachineBrain cinemachineBrain;
-    
+    private Animator anim;
+    float start;
     
     void Start()
     {
+        anim = GetComponent<Animator>();
         playerInputController = GetComponent<CharacterController>();
         jumpVelocity = Mathf.Sqrt(-2 * gravityOnPlayer * jumpHeight);
         cinemachineBrain = Camera.main.GetComponent<CinemachineBrain>();
@@ -35,6 +41,12 @@ public class MovementManager : MonoBehaviour
 
     void Update()
     {
+        if (playerInputController.transform.position.z > 10){
+            Debug.Log(Time.time - start);
+        }
+        if (Input.GetKeyDown(KeyCode.W)){
+            start = Time.time;
+        }
         grounded = IsPlayerOnGround();
         MovePlayerFromDirection();
         PlayerGroundGravityAffect();
@@ -56,40 +68,76 @@ public class MovementManager : MonoBehaviour
 
         dashCooldownTimer -= Time.deltaTime;
     }
-
-    void MovePlayerFromDirection()
+public float movementLerpSpeed = 10f;
+public float decelerationLerpSpeed = 0.5f; // Added deceleration lerp speed
+Vector3 playerVelocity;
+       
+void MovePlayerFromDirection()
 {
-    if (!isMovementLocked){
-        playerHorizontalInput = 0;
-    playerVerticalInput = 0;
-
-    if (Input.GetKey(KeyCode.W))
+    if (!isMovementLocked)
     {
-        playerVerticalInput = 1;
+        // Input handling
+        float playerVerticalInput = 0;
+        float playerHorizontalInput = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerVerticalInput = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            playerVerticalInput = -1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerHorizontalInput = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerHorizontalInput = 1;
+        }
+
+        // Animation control
+        if (playerVerticalInput == 1 && playerHorizontalInput == 0)
+        {
+            Debug.Log("moving forward");
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+
+        // Calculate movement direction and speed
+        float movementSpeed = IsPlayerOnGround() ? playerMovementSpeed : playerMovementSpeed * 0.9f;
+        Vector3 playerMovementDirection = (transform.forward * playerVerticalInput + transform.right * playerHorizontalInput).normalized;
+        Vector3 targetVelocity = playerMovementDirection * movementSpeed;
+
+        // Smoothly lerp between current velocity and target velocity
+        playerVelocity = Vector3.Lerp(playerVelocity, targetVelocity, movementLerpSpeed * Time.deltaTime);
+
+        // Apply movement
+        playerInputController.Move(playerVelocity * Time.deltaTime);
     }
-    else if (Input.GetKey(KeyCode.S))
+    else
     {
-        playerVerticalInput = -1;
+        Debug.Log("teleporting...");
     }
 
-    if (Input.GetKey(KeyCode.A))
+    // Decelerate when no movement keys are pressed
+    if (playerVerticalInput == 0 && playerHorizontalInput == 0)
     {
-        playerHorizontalInput = -1;
-    }
-    else if (Input.GetKey(KeyCode.D))
-    {
-        playerHorizontalInput = 1;
-    }
-
-    movementSpeed = IsPlayerOnGround() ? playerMovementSpeed : playerMovementSpeed * 0.9f;
-
-    playerMovementDirection = (transform.forward * playerVerticalInput + transform.right * playerHorizontalInput).normalized;
-
-    playerInputController.Move(playerMovementDirection * movementSpeed * Time.deltaTime);
-    } else {
-        Debug.Log("teleportning...");
+        if (grounded){
+            movementLerpSpeed = 15f;
+        } else {
+            movementLerpSpeed = 5f;
+        }
+        playerVelocity = Vector3.Lerp(playerVelocity, Vector3.zero, decelerationLerpSpeed * Time.deltaTime);
+        playerInputController.Move(playerVelocity * Time.deltaTime);
     }
 }
+
     void PlayerVerticalJumpMovement()
     {
         if (grounded)
@@ -197,15 +245,11 @@ Vector3 GetDashDirection()
 {
     Vector3 dashDirection = Vector3.zero;
 
-    // Check for input keys
-    if (Input.GetKey(KeyCode.W))
-        dashDirection += transform.forward;
-    if (Input.GetKey(KeyCode.D))
-        dashDirection += transform.right;
-    if (Input.GetKey(KeyCode.S))
-        dashDirection -= transform.forward;
-    if (Input.GetKey(KeyCode.A))
-        dashDirection -= transform.right;
+    // Check for input keys and update dashDirection accordingly
+    dashDirection += (Input.GetKey(KeyCode.W) ? transform.forward : Vector3.zero);
+    dashDirection += (Input.GetKey(KeyCode.D) ? transform.right : Vector3.zero);
+    dashDirection -= (Input.GetKey(KeyCode.S) ? transform.forward : Vector3.zero);
+    dashDirection -= (Input.GetKey(KeyCode.A) ? transform.right : Vector3.zero);
 
     // If no directional input, default to forward
     if (dashDirection.magnitude == 0)
