ec120c8c23fdb737cc16672f458ed62a654a9c8d
DapZey <72968513+DapZey@users.noreply.github.com>
2024-02-19 11:50:54 +0200

    Update NewMovement.cs

--- NewMovement.cs
+++ NewMovement.cs
@@ -1,107 +1,137 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using System;
-using Palmmedia.ReportGenerator.Core.Parser.Analysis;
-
-public class NewMovement : MonoBehaviour
+public class Logs : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private float _jumpHeight;
+    CharacterController _playerInputController;
+
+    private float _gravityOnPlayer;
+    private float _jumpVelocity;
+    CinemachineBrain _cinemachineBrain;
+    private Animator _anim;
+    private Vector3 _vectorToMove;
+    private bool _isMovementLocked;
+    private float _movementLerpSpeed = 2f;
+    private float _decelerationLerpSpeed = 0.5f; // Added deceleration lerp speed
+    Vector3 _playerVelocity;
     public float playerMovementSpeed = 5;
-    public float dashSpeed = 250f;
+    private bool _grounded;
+    private int _runHash;
+    private Transform _transform;
+    private float _movingDelta;
+    private float _fallingDelta;
+    private float _jumpingDelta;
+    Vector3 _dashMovement;
+    private int _framesLeftForDash;
+    float _dashStartTime;
+    int _totalFramesForDash = 300; 
+    float _ms = 0.001f;
+    float _vectorMovedSumZ;
+    float _vectorMovedSumX;
+    Vector3 _dashTrajectory;
     public float dashCooldown = 1;
-    private float dashCooldownTimer = 0;
-
-    float playerHorizontalInput;
-    float playerVerticalInput;
-    [HideInInspector] public Vector3 playerMovementDirection;
-
-    public float jumpHeight;
-    CharacterController playerInputController;
-    [SerializeField] LayerMask playerGroundMask;
-
-    float gravityOnPlayer = -20f;
-    Vector3 playerMovementVelocity;
-    bool grounded;
-    float movementSpeed;
-    bool isMovementLocked = false;
-     Vector3 dashMovement;
-    int framesLeftForDash = 50000001;
-    float dashStartTime;
-    int totalFramesForDash = 300; 
-    float ms = 0.001f;
-    float vectorMovedSumZ = 0;
-    float vectorMovedSumX = 0;
-    Vector3 dashTrajectory;
+    private float _dashCooldownTimer;
+    Vector3 _playerMovementVelocity;
+    
     void Start()
     {
-        playerInputController = GetComponent<CharacterController>();
+        _framesLeftForDash  = 50000001;
+        _transform = this.transform;
+        _isMovementLocked = false;
+        _jumpHeight = 2f;
+        _anim = GetComponent<Animator>();
+        _gravityOnPlayer = -30f;
+        _playerInputController = GetComponent<CharacterController>();
+        _jumpVelocity = Mathf.Sqrt(-2 * _gravityOnPlayer * _jumpHeight);
+        _cinemachineBrain = GetComponent<CinemachineBrain>();
+        _runHash = Animator.StringToHash("Run");
     }
 
     // Update is called once per frame
     void Update()
-{
-    Vector3 vectorToMove = Vector3.zero;
-    // Add movement from player input
-    if (Input.GetKeyDown(KeyCode.LeftShift))
     {
-        isMovementLocked = true;
-        vectorMovedSumZ = 0;
-        vectorMovedSumX = 0;
-        framesLeftForDash = 0;
-        dashStartTime = Time.time;
-        dashTrajectory = CalculateDashMovement();
-        dashMovement = dashTrajectory / totalFramesForDash;
-        Debug.Log(dashTrajectory);
+        _jumpingDelta = Time.deltaTime;
+        _movingDelta = Time.deltaTime;
+        _fallingDelta = Time.deltaTime;
+        if (_fallingDelta > 0.04f)
+        {
+            _fallingDelta = (_fallingDelta * 2f) -0.04f;
+        }
+        if (_movingDelta > 0.04f)
+        {
+            _movingDelta = 0.04f;
+        }
+
+        if (_playerInputController.transform.position.y < 6.28f)
+        {
+            Debug.Log(Time.time);
+        }
+        Vector3 playerMovement = Vector3.zero;
+        _grounded = IsPlayerOnGround();
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _isMovementLocked = true;
+            _vectorMovedSumZ = 0;
+            _vectorMovedSumX = 0;
+            _framesLeftForDash = 0;
+            _dashStartTime = Time.time;
+            _dashTrajectory = CalculateDashMovement();
+            _dashMovement = _dashTrajectory / _totalFramesForDash;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           playerMovement += PlayerVerticalJumpMovement();
+        }
+        playerMovement +=CheckDash(playerMovement);
+        playerMovement +=MovePlayerFromDirection();
+        playerMovement += PlayerGroundGravityAffect();
+        _playerInputController.Move(playerMovement);
     }
-    vectorToMove +=checkDash(vectorToMove);
-    vectorToMove += MovePlayerFromDirection();
-      playerInputController.Move(vectorToMove);
-}
-Vector3 checkDash(Vector3 vectorToMove){
-    if (framesLeftForDash < totalFramesForDash)
+    Vector3 PlayerVerticalJumpMovement()
     {
-        float elapsedTime = Time.time - dashStartTime;
-        float inc =(elapsedTime / ms)-framesLeftForDash;
-        framesLeftForDash += Mathf.FloorToInt(inc);
-        if (framesLeftForDash > 0 && vectorMovedSumZ + (dashMovement * Mathf.FloorToInt(inc)).z <= MathF.Abs(dashTrajectory.z) && vectorMovedSumZ + (dashMovement * Mathf.FloorToInt(inc)).z >= -MathF.Abs(dashTrajectory.z)
-        && vectorMovedSumX + (dashMovement * Mathf.FloorToInt(inc)).x <= MathF.Abs(dashTrajectory.x) && vectorMovedSumX + (dashMovement * Mathf.FloorToInt(inc)).x >= -MathF.Abs(dashTrajectory.x)){
+        if (_grounded)
+        {
+            if (_jumpingDelta < 0.04f)
+            {
+                _playerMovementVelocity.y = _jumpVelocity;
+            }
+            else
+            {
+                _playerMovementVelocity.y = _jumpVelocity /100f;
+            }
+        }
+
+        return _playerMovementVelocity * _jumpingDelta;
+    }
+    Vector3 PlayerGroundGravityAffect()
+    {
+        if (!_grounded)
+        {
+            _playerMovementVelocity.y += _gravityOnPlayer * _fallingDelta;
+        }
+        else if (_playerMovementVelocity.y < 0)
+        {
+            _playerMovementVelocity.y = -2f;
             
-                vectorToMove += dashMovement * Mathf.FloorToInt(inc);
-                vectorMovedSumZ += vectorToMove.z;
-                vectorMovedSumX += vectorToMove.x;
-                Debug.Log(vectorMovedSumX);
-                Debug.Log(vectorMovedSumZ);
         }
-        else if (framesLeftForDash > 0){
-            if (vectorMovedSumZ > 0){
-                vectorToMove.z += MathF.Abs(dashTrajectory.z)-vectorMovedSumZ;
-            }
-            else{
-                vectorToMove.z += -MathF.Abs(dashTrajectory.z) + Math.Abs( vectorMovedSumZ);
-            }
-            if (vectorMovedSumX > 0){
-                vectorToMove.x += MathF.Abs(dashTrajectory.x)-vectorMovedSumX;
-            }
-            else{
-                vectorToMove.x += -MathF.Abs(dashTrajectory.x) + Math.Abs( vectorMovedSumX);
-            }
-            vectorMovedSumZ += vectorToMove.z;
-            vectorMovedSumX += vectorToMove.x;
-        }
+
+        return _playerMovementVelocity * _fallingDelta;
     }
-    else {
-        isMovementLocked = false;
+    bool IsPlayerOnGround()
+    {
+        return _playerInputController.isGrounded;
     }
-    return vectorToMove;
-}
     Vector3 MovePlayerFromDirection()
     {
-        if (!isMovementLocked)
+        Vector3 toMove = Vector3.zero;
+        float playerVerticalInput = 0;
+        float playerHorizontalInput = 0;
+        if (!_isMovementLocked)
         {
-            playerHorizontalInput = 0;
-            playerVerticalInput = 0;
-
+            
+    
             if (Input.GetKey(KeyCode.W))
             {
                 playerVerticalInput = 1;
@@ -110,7 +140,7 @@ Vector3 checkDash(Vector3 vectorToMove){
             {
                 playerVerticalInput = -1;
             }
-
+    
             if (Input.GetKey(KeyCode.A))
             {
                 playerHorizontalInput = -1;
@@ -119,64 +149,114 @@ Vector3 checkDash(Vector3 vectorToMove){
             {
                 playerHorizontalInput = 1;
             }
-
-            movementSpeed = grounded ? playerMovementSpeed : playerMovementSpeed * 0.9f;
-
-            Vector3 movementVector = (transform.forward * playerVerticalInput + transform.right * playerHorizontalInput).normalized * movementSpeed * Time.deltaTime;
-            playerMovementDirection = movementVector;
-            return movementVector;
+    
+            // Animation control
+            if (playerVerticalInput == 1 && playerHorizontalInput == 0)
+            {
+                _anim.SetBool(_runHash, true);
+            }
+            else
+            {
+                _anim.SetBool(_runHash, false);
+            }
+    
+            // Calculate movement direction and speed
+            float movementSpeed = IsPlayerOnGround() ? playerMovementSpeed : playerMovementSpeed * 0.9f;
+            Vector3 playerMovementDirection = (_transform.forward * playerVerticalInput + _transform.right * playerHorizontalInput).normalized;
+            Vector3 targetVelocity = playerMovementDirection * movementSpeed;
+    
+            // Smoothly lerp between current velocity and target velocity
+            _playerVelocity = Vector3.Lerp(_playerVelocity, targetVelocity, _movementLerpSpeed * _movingDelta);
+    
+            // Apply movement
+            toMove += _playerVelocity * _movingDelta;
         }
-        else
+            if (_grounded){
+                _movementLerpSpeed = 15f;
+            } else {
+                _movementLerpSpeed = 2f;
+            }
+            _playerVelocity = Vector3.Lerp(_playerVelocity, Vector3.zero, _decelerationLerpSpeed * _movingDelta);
+            toMove += _playerVelocity * _movingDelta;
+            return toMove;
+    }
+    Vector3 CheckDash(Vector3 vectorToMove){
+        if (_framesLeftForDash < _totalFramesForDash)
         {
-            Debug.Log("locked");
-            return Vector3.zero;
+            float elapsedTime = Time.time - _dashStartTime;
+            float inc =(elapsedTime / _ms)-_framesLeftForDash;
+            _framesLeftForDash += Mathf.FloorToInt(inc);
+            if (_framesLeftForDash > 0 && _vectorMovedSumZ + (_dashMovement * Mathf.FloorToInt(inc)).z <= MathF.Abs(_dashTrajectory.z) && _vectorMovedSumZ + (_dashMovement * Mathf.FloorToInt(inc)).z >= -MathF.Abs(_dashTrajectory.z)
+                && _vectorMovedSumX + (_dashMovement * Mathf.FloorToInt(inc)).x <= MathF.Abs(_dashTrajectory.x) && _vectorMovedSumX + (_dashMovement * Mathf.FloorToInt(inc)).x >= -MathF.Abs(_dashTrajectory.x)){
+            
+                vectorToMove += _dashMovement * Mathf.FloorToInt(inc);
+                _vectorMovedSumZ += vectorToMove.z;
+                _vectorMovedSumX += vectorToMove.x;
+            }
+            else if (_framesLeftForDash > 0){
+                if (_vectorMovedSumZ > 0){
+                    vectorToMove.z += MathF.Abs(_dashTrajectory.z)-_vectorMovedSumZ;
+                }
+                else{
+                    vectorToMove.z += -MathF.Abs(_dashTrajectory.z) + Math.Abs( _vectorMovedSumZ);
+                }
+                if (_vectorMovedSumX > 0){
+                    vectorToMove.x += MathF.Abs(_dashTrajectory.x)-_vectorMovedSumX;
+                }
+                else{
+                    vectorToMove.x += -MathF.Abs(_dashTrajectory.x) + Math.Abs( _vectorMovedSumX);
+                }
+                _vectorMovedSumZ += vectorToMove.z;
+                _vectorMovedSumX += vectorToMove.x;
+            }
         }
+        else {
+            _isMovementLocked = false;
+        }
+        return vectorToMove;
     }
-   Vector3 CalculateDashMovement()
-{
-    dashCooldownTimer = dashCooldown;
-
-    // Define dash parameters
-    float dashDuration = 0.3f;
-    Vector3 dashDirection = GetDashDirection();
-
-    if (dashDirection.magnitude > 0)
+    Vector3 CalculateDashMovement()
     {
-        dashDirection = dashDirection.normalized;
+        _dashCooldownTimer = dashCooldown;
 
-        // Calculate total movement for the entire dash duration
-        Vector3 totalMovement = 18.0f * dashDirection * dashDuration;
+        // Define dash parameters
+        float dashDuration = 0.3f;
+        Vector3 dashDirection = GetDashDirection();
 
-        // Reset the player's velocity after the dash
-        playerMovementVelocity = totalMovement;
+        if (dashDirection.magnitude > 0)
+        {
+            dashDirection = dashDirection.normalized;
 
-        return totalMovement;
+            // Calculate total movement for the entire dash duration
+            Vector3 totalMovement = dashDirection * dashDuration;
+
+            return totalMovement * 18f;
+        }
+
+        // If no dash direction is provided, return zero vector
+        return Vector3.zero;
     }
 
-    // If no dash direction is provided, return zero vector
-    return Vector3.zero;
-}
 
 
+    Vector3 GetDashDirection()
+    {
+        Vector3 dashDirection = Vector3.zero;
 
-Vector3 GetDashDirection()
-{
-    Vector3 dashDirection = Vector3.zero;
+        // Check for input keys
+        if (Input.GetKey(KeyCode.W))
+            dashDirection += transform.forward;
+        if (Input.GetKey(KeyCode.D))
+            dashDirection += transform.right;
+        if (Input.GetKey(KeyCode.S))
+            dashDirection -= transform.forward;
+        if (Input.GetKey(KeyCode.A))
+            dashDirection -= transform.right;
 
-    // Check for input keys
-    if (Input.GetKey(KeyCode.W))
-        dashDirection += transform.forward;
-    if (Input.GetKey(KeyCode.D))
-        dashDirection += transform.right;
-    if (Input.GetKey(KeyCode.S))
-        dashDirection -= transform.forward;
-    if (Input.GetKey(KeyCode.A))
-        dashDirection -= transform.right;
+        // If no directional input, default to forward
+        if (dashDirection.magnitude == 0)
+            dashDirection = transform.forward;
 
-    // If no directional input, default to forward
-    if (dashDirection.magnitude == 0)
-        dashDirection = transform.forward;
-
-    return dashDirection;
-}
+        return dashDirection;
+    }
 }
